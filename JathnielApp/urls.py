07e5605bb10399600c1a7de73985e8e98ffa0ee2
D3stinn3 <paynedestinne@gmail.com>
2023-09-22 12:18:37 +0300

    First Launch

--- JathnielApp/urls.py
+++ JathnielApp/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from JathnielApp import views
 
 urlpatterns = [
+    path('home/', views.homePage, name="home")
 ]
--- JathnielApp/views.py
+++ JathnielApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homePage(request):
+    context = {}
+    return render(request, 'temp/base.html', context)
