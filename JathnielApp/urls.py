f2a3487dcc1855b3b59d3fd0da5f0d4de83cd692
D3stinn3 <paynedestinne@gmail.com>
2023-09-27 19:29:12 +0300

    Urls

--- JathnielApp/urls.py
+++ JathnielApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from JathnielApp import views
 
 urlpatterns = [
-    path('', views.homePage, name="home"),
+    path('home/', views.homePage, name="home"),
     path('index/', views.indexPage, name="index"),
     path('contact/', views.contactPage, name="contact"),
     path('about/', views.aboutPage, name="about"),
