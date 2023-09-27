e390bf5af9ada6cba328c6a94ac3cfe8932792d5
D3stinn3 <paynedestinne@gmail.com>
2023-09-27 19:28:08 +0300

    Last Last

--- ProjectJathniel/settings.py
+++ ProjectJathniel/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'theme' / 'static'
 ]
-STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
+STATIC_ROOT = BASE_DIR.parent / 'local-cdn' / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
