17d4eb560df22a46ae923a66720fe7519b6e495a
D3stinn3 <paynedestinne@gmail.com>
2023-12-21 18:14:07 +0300

    Views

--- JathnielApp/views.py
+++ JathnielApp/views.py
@@ -4,7 +4,7 @@ from JathnielApp.forms import CommentsForm
 from JathnielApp.models import Comments
 from twilio.rest import Client
 import time
-
+from twilio.base.exceptions import TwilioRestException
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
@@ -14,19 +14,29 @@ auth_token = 'a55f7a98613ec3e5a9acbd93e0fd365b'
 
 
 def send_whatsapp_message(comment):
-    # Replace these with your Twilio credentials
-    account_sid = 'AC2e7334957c5e7973449d5af02311d3b0'
-    auth_token = 'a55f7a98613ec3e5a9acbd93e0fd365b'
-    twilio_phone_number = 'whatsapp:+14155238886'
-    recipient_phone_number = 'whatsapp:+254768507237'  # Replace with the recipient's number
+    try:
+        # Replace these with your Twilio credentials
+        account_sid = 'your_account_sid'
+        auth_token = 'your_auth_token'
+        twilio_phone_number = 'whatsapp:+14155238886'
+        recipient_phone_number = 'whatsapp:+254708526536'  # Replace with the recipient's number
 
-    client = Client(account_sid, auth_token)
+        client = Client(account_sid, auth_token)
 
-    message = client.messages.create(
-        from_=twilio_phone_number,
-        body=f"(Message: {comment})",
-        to=recipient_phone_number
-    )
+        message = client.messages.create(
+            from_=twilio_phone_number,
+            body=f"New Comment by: {comment.email} (Message: {comment.message})",
+            to=recipient_phone_number
+        )
+
+        # Check if the message was successfully sent
+        if message.error_code is not None:
+            # Log the error or handle it accordingly
+            print(f"Error sending WhatsApp message: {message.error_message}")
+
+    except TwilioRestException as e:
+        # Log the exception or handle it accordingly
+        print(f"Twilio error: {e}")
 
 # Create your views here.
 def homePage(request):
